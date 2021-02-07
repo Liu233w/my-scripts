23e7528da039a1feeb0044d0090711483de42c7a
Liu233w <Liu233w@users.noreply.github.com>
2021-02-07 15:01:11 +1100

    fix

--- cookie-clicker-addon.js
+++ cookie-clicker-addon.js
@@ -1,1 +1,14 @@
-Game.playCookieClickSound = ()=>{return}
+
+function liu233wInit() {
+  const timer = setInterval(() => {
+    if (Game && Game.playCookieClickSound) {
+      console.log('Disabling cookie clicking sound')
+      Game.playCookieClickSound = ()=>{return}
+
+      // cleanup
+      clearInterval(timer)
+    } else {
+      console.log('waiting game loaded')
+    }
+  }, 500)
+}
